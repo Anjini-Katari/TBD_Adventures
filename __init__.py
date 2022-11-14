36d48240e0d09c55566375764f73bde0922e2b36
Anjini Katari <akatari30@stuy.edu>
2022-11-13 23:53:05 -0500

    crying but it works...ish

--- __init__.py
+++ __init__.py
@@ -13,11 +13,12 @@ import sqlite3
 
 DB_FILE="users.db"
 
-db = sqlite3.connect(DB_FILE) #open if file exists, otherwise create
+db = sqlite3.connect(DB_FILE, check_same_thread=False) #open if file exists, otherwise create
 c = db.cursor()               #facilitate db ops -- you will use cursor to trigger db events
 
 # Initializing db
-c.execute("CREATE TABLE IF NOT EXISTS users(username TEXT, password TEXT;")
+c.execute("CREATE TABLE IF NOT EXISTS users(username TEXT, password TEXT)")
+db.commit()
 
 
 
@@ -36,37 +37,51 @@ def disp_loginpage():
 def addNewUser():
     msg = "An error occurred. Try again."
     if request.method == 'POST':
-        if 'username' in request.form and 'password' in request.form:
+#        if 'username' in request.form and 'password' in request.form:
             # need to check the input to make sure it's valid
-            c.execute("INSERT INTO user VALUES('{}', '{}')".format(i['username'], i['password'])) # adds user pass combo into the db
+            c.execute("INSERT INTO users VALUES('{}', '{}')".format(request.form['username'],request.form['password'])) # adds user pass combo into the db
             #            c.execute("INSERT INTO user VALUES('{}', '{}')".format(i['username'], i['password'])) # adds user pass combo into the db
+            db.commit() #saves the inputs to the db
             print(c.execute("SELECT * from users"))
     return render_template('login.html', msg=msg)
+
+@app.route("/signUp", methods=['GET','POST'])
+def signUp():
+    
+    return render_template('signup.html')
+
+# going to the sign up page to collect the new username and password is a different step from going to the login page after 
+# the values have been submitted which is why we need two app routes 
 
 @app.route("/login", methods =['GET', 'POST'])
 def authenticate():
     msg = ""
 
-    session['username'] = "x" #hard coded username and password
-    session['password'] = "y"
+    pwd_check = c.execute("SELECT password from users where username = '{}'".format(request.form['username']))
+    if pwd_check == request.form['password']:
+        session['username']=request.form['username']
+        session['password']=request.form['password']
+    else:
+        session['username']='None'
+        session['password']='None'
 
-    if 'username' in request.form: # error otherwise because request.form could be empty
+    # if 'username' in request.form: # error otherwise because request.form could be empty
 
-        if session['username'] == request.form['username']: # assumes username exists within session (it does rn because we hard code it)
-            if session['password'] == request.form['password']:
-                msg = "you're logged in"
-                return render_template( 'user.html', user=(request.form['username']))  # response to a form submission
-            else:
-                msg = "you're not logged in because your password was wrong"
-                return render_template( 'login.html', msg=msg)
-        else:
-            if session['password'] == request.form['password']:
-                msg = "you're not logged in because your username was wrong"
-            else:
-                msg = "you're not logged in because your username and password were wrong"
-            return render_template( 'login.html', msg=msg)
+    #     if session['username'] == request.form['username']: # assumes username exists within session (it does rn because we hard code it)
+    #         if session['password'] == request.form['password']:
+    #             msg = "you're logged in"
+    #             return render_template( 'user.html', user=(request.form['username']))  # response to a form submission
+    #         else:
+    #             msg = "you're not logged in because your password was wrong"
+    #             return render_template( 'login.html', msg=msg)
+    #     else:
+    #         if session['password'] == request.form['password']:
+    #             msg = "you're not logged in because your username was wrong"
+    #         else:
+    #             msg = "you're not logged in because your username and password were wrong"
+    #         return render_template( 'login.html', msg=msg)
 
-    return render_template( 'login.html', msg="Sorry, an error occured. Try again.") # should not ever get to this point but just in case it does
+    return render_template( 'home.html', msg="", user=session['username']) 
 
 
 
